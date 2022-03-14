ca1e4c92ec26f514081ed42890386fe204385f1b
ethansnow <ethansnow2012@gmail.com>
2022-03-14 19:21:24 +0800

    chore: --

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 
 
 import './App.css';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MainPage } from './container/MainPage'
 import { AboutPage } from './container/AboutPage'
 import { ExamplePage } from 'container/ExamplePage';
@@ -11,13 +11,15 @@ import { ExamplePage } from 'container/ExamplePage';
 
 function App() {
   return (
-    <Router>
+    <div>    
+    <BrowserRouter basename="/rdrag-rdrop">
       <Routes>
         <Route path='/' element={<MainPage />}></Route>
         <Route path='/about' element={<AboutPage />}></Route>
         <Route path='/example' element={<ExamplePage />}></Route>
       </Routes>
-    </Router>
+    </BrowserRouter>  
+    </div>
   );
 }
 
