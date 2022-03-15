35f7195d90cc1f1adbfbd9e75187de3814415fe0
ethansnow <ethansnow2012@gmail.com>
2022-03-15 12:35:01 +0800

    refactor: break down MainPage

--- src/container/MainPage/index.js
+++ src/container/MainPage/index.js
@@ -1,4 +1,4 @@
-import bg_1 from 'assets/bg-1.jpg';
+
 import bg_2 from 'assets/bg-2.jpg';
 import bg_3 from 'assets/bg-3.jpg';
 
@@ -11,128 +11,20 @@ import {RDragAndDropWrapper, DefaultStyle as RDragAndDropWrapperStyle} from 'con
 import {RDragAndDropTarget} from 'component/RDragAndDropTarget'
 import {dragAndDropUtils, dragAndDropContext} from 'rDragAndDrop/index'
 
+import {PageSection_1} from './PageSection_1/index'
+
 
 
 const Styled = styled.div`
-    & ${StaticBackgroundBlockStyle}{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-    }
-    & ${StaticBackgroundBlockStyle} ${RDragAndDropWrapperStyle} {
-        display: flex;
-        justify-content: center;
-        //align-items: center;
-        flex-direction: column;
-    }
-
-    & ${StaticBackgroundBlockStyle} ${RDragAndDropWrapperStyle} .p-column-inner{
-        display: flex;
-        flex-direction: column;
-        align-items: stretch;
-    }
-
-    & ${StaticBackgroundBlockStyle} ${RDragAndDropWrapperStyle} .p-column-inc{
-        width: 100%;
-        margin-top:auto;
-        min-width:110px
-    }
-    & ${StaticBackgroundBlockStyle} ${RDragAndDropWrapperStyle} .p-column-inc-add{
-        width: 100%;
-        height: 30px;
-        background: grey;
-        justify-content: center;
-        align-items: center;
-        display:flex;
-    }
-    & ${StaticBackgroundBlockStyle} ${RDragAndDropWrapperStyle} .p-column-inc-add::before{
-        content:'+';
-        color: white;
-        position:relative;
-        top:-2px;
-        font-size: 23px;
-        left:-1px;
-    }
-    .rDragAndDropRoot-wrapper{
-        width: calc(100% - 10vh);
-        height: calc(100vh - 20vh);
-        display: flex;
-        justify-content: center;
-        max-width: 100%;
-        overflow: auto;
-    }
+    
 `
 
 export function MainPage() {
-    let refsMap = useRef(new Map()).current;
-    const [todoData, setTodoData] = useState({data:[]})
-
-    // Closure:refsMap, todoData
-    const setRefsMap = function(columnData){
-        return (ref) => {
-            //console.log('setRefsMap', columnData, ref)
-            return ref === null ? refsMap.delete(columnData.id) : refsMap.set(columnData.id, ref)
-        }
-    }
-    
-
-    const dragAndDropContextInstance = dragAndDropUtils.initContext()()
-    const contextObject = {
-        data: todoData, 
-        setData: setTodoData, 
-        contextInstance: dragAndDropContextInstance
-    }
-
-    // Closure:refsMap
-    const logRef = (id)=>{
-        return async ()=>{
-            const dueRef = refsMap.get(id)
-            console.log('logRef', dueRef)
-            const newData = await getNewOneTodoData()
-            dueRef.updateOneTodo(newData)
-        }
-    }
-    useEffect(async ()=>{
-        setTodoData(await getAllTodoData().then((data)=>{console.log('a', data); return data }))
-    },[])
     return (
         <Styled>
-        <StaticBackgroundBlock image={bg_1}>
-            <dragAndDropContext.Provider value={contextObject}>   
-                <div className='rDragAndDropRoot-wrapper'>
-                    <RDragAndDropRoot >
-                        {
-                            todoData.data
-                                ?.map((columnData,ii) =>
-                                {
-                                    return(    
-                                        <RDragAndDropWrapper 
-                                            ref={setRefsMap(columnData)}
-                                            data-foo={ii} key={columnData.id} self={columnData} parent={todoData.data} >
-                                            <div className='p-column-inner'>
-                                                {
-                                                    columnData.data
-                                                        .map((todoItem, jj)=>(
-                                                            <RDragAndDropTarget key={todoItem.id} self={todoItem} parent={columnData}/>
-                                                        ))
-                                                }
-                                            </div>
-                                            <div className='p-column-inc'>
-                                                <div className='p-column-inc-add' onClick={logRef(columnData.id)}>
-                                                </div>
-                                            </div>
-                                        </RDragAndDropWrapper>
-                                    )
-                                }
-                            )
-                        }
-                    </RDragAndDropRoot>
-                </div>
-            </dragAndDropContext.Provider>
-        </StaticBackgroundBlock>
-        <StaticBackgroundBlock image={bg_2}/>
-        <StaticBackgroundBlock image={bg_3}/>
+            <PageSection_1/>
+            <StaticBackgroundBlock image={bg_2}/>
+            <StaticBackgroundBlock image={bg_3}/>
         </Styled>
     )
 }
