00e13271e1d9fec4492ccc35a63e3b83798d38c1
ethansnow <ethansnow2012@gmail.com>
2022-03-19 01:06:34 +0800

    feat: github page spa routes

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import logo from './logo.svg';
 
 
 import './App.css';
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route,useNavigate } from 'react-router-dom'
 import { MainPage } from './container/MainPage'
 import { AboutPage } from './container/AboutPage'
 import { ExamplePage } from 'container/ExamplePage';
+import GitHubPageRoute from 'hoc/router/GithubRoute'
 
-
-
+let NODE_ENV = process.env.REACT_APP_NODE_ENV
+NODE_ENV = 'github_page'
+const baseName = NODE_ENV=='github_page'?'/rdrag-rdrop':''
 function App() {
+  console.log('ssfff', NODE_ENV, baseName)
+  
   return (
-    <div> 
-    <BrowserRouter basename={process.env.REACT_APP_NODE_ENV=='github_page'?'/rdrag-rdrop':''}>
+    
+    <BrowserRouter >
       
-      <Routes>
-        <Route path='/' element={<MainPage />}></Route>
-        <Route path='/about' element={<AboutPage />}></Route>
-        <Route path='/example' element={<ExamplePage />}></Route>
+      <Routes basename={baseName}>
+        <Route path={baseName+'/'} element={GitHubPageRoute(MainPage, baseName)}></Route>
+        <Route path={baseName+'/about'} element={GitHubPageRoute(AboutPage, baseName)}></Route>
+        <Route path={baseName+'/example'} element={GitHubPageRoute(ExamplePage, baseName)}></Route>
       </Routes>
     </BrowserRouter>  
-    </div>
+    
   );
 }
 
