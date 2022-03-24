5fc405221eeb7bd6ef5006ba458d1485193a3514
ethansnow <ethansnow2012@gmail.com>
2022-03-24 12:11:10 +0800

    refactor: scrollHandle not re-init

--- src/container/GlobalWorker/index.js
+++ src/container/GlobalWorker/index.js
@@ -5,21 +5,24 @@ import {useGlobalState} from 'context/GlobalStateProvider'
 
 export function GlobalWorker() {
     const [state, dispatch] = useGlobalState()
-
+    
     var lastScrollTop = 0    
-    const scrollHandle = ()=>{
-      var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-      if (st > lastScrollTop){
-        if(state.scrollDirection!='down'){
-            dispatch({...state, scrollDirection:'down'})
+    const scrollHandle = useCallback(()=>{
+        const [state, dispatch] = useGlobalState()
+        var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+        if (st > lastScrollTop){
+          if(state.scrollDirection!='down'){
+              dispatch({...state, scrollDirection:'down'})
+          }
+        } else {
+          if(state.scrollDirection!='up'){
+              dispatch({...state, scrollDirection:'up'})
+          }
         }
-      } else {
-        if(state.scrollDirection!='up'){
-            dispatch({...state, scrollDirection:'up'})
-        }
-      }
-      lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-    }
+        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+      }, []);
+
+    
     useEffect(() => {
       console.log('rehook')
       function watchScroll() {
