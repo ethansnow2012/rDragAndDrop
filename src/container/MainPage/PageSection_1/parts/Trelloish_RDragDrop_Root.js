a912047309a5c77dd258f070d46e88cc56a83779
ethansnow <ethansnow2012@gmail.com>
2022-03-24 17:31:40 +0800

    chore: style

--- src/container/MainPage/PageSection_1/parts/Trelloish_RDragDrop_Root.js
+++ src/container/MainPage/PageSection_1/parts/Trelloish_RDragDrop_Root.js
@@ -7,8 +7,10 @@ import {rDragRDropContext, rDragRDrop} from 'rDragRDrop/index'
 const Styled = styled.div`
     background: rgba(255, 255, 255, 0.2);
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(5px);
-    -webkit-backdrop-filter: blur(5px);
+    /*
+        bug: scroll ... flickered.
+        backdrop-filter: blur(5px);    
+    */
     display: flex;
     padding: 1em 2em;
     & > ${DefaultStyle}{
--- src/container/MainPage/PageSection_2/parts/Post_RDragDrop_Root.js
+++ src/container/MainPage/PageSection_2/parts/Post_RDragDrop_Root.js
@@ -5,8 +5,10 @@ import {rDragRDropContext, rDragRDrop} from 'rDragRDrop/index'
 const Styled = styled.div`
     background: rgba(255, 255, 255, 0.2);
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(5px);
-    -webkit-backdrop-filter: blur(5px);
+    /*
+        bug: scroll ... flickered.
+        backdrop-filter: blur(5px);    
+    */
     padding: 1em 2em;
     min-width:90vw;
     min-height:100vh;
