7a63c4b4962f7aabffb7cdf95fcef5811d9c25ed
ethansnow <ethansnow2012@gmail.com>
2022-03-24 20:52:53 +0800

    feat: init tests

---
+++ src/components/HeaderForPopup/index.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import { HeaderForPopupH1, HeaderForPopupH2 } from './index.js'
+
+test('HeaderForPopup render text', ()=>{
+    const renderRTN = render(
+        <div>
+            <HeaderForPopupH2>fdhdfhfdhj</HeaderForPopupH2>
+            <HeaderForPopupH1>aagfsdgsdfgaa</HeaderForPopupH1>
+        </div>
+    )
+    //console.log(renderRTN)
+    expect(screen.getByText('aagfsdgsdfgaa')).toBeDefined();
+    expect(screen.getByText('fdhdfhfdhj')).toBeDefined();
+})
---
+++ src/components/popup/index.test.js
@@ -0,0 +1,34 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import { DefaultPopup } from './index.js'
+import styled from 'styled-components'
+import {useState} from 'react'
+
+
+const portalStyled = styled.div`
+    .p-codepopup > * + *{
+        margin-top:1.5rem;
+    }
+`
+
+function TestComponent({ url }) {
+    const [popupState, setPopupState] = useState(true)
+    return (  
+        <DefaultPopup portalStyled={portalStyled} portalTarget={'body'} popupState={[popupState, setPopupState]} className='p-codepopup'></DefaultPopup>
+    )
+}
+
+function renderMyPortal() {
+    const renderUtils = render(<TestComponent />)
+
+    return renderUtils
+  }
+
+beforeEach(cleanup)
+
+test('HeaderForPopup render text', ()=>{
+    const {unmount} = renderMyPortal(<TestComponent />)
+    
+    expect(document.body.querySelector('.p-codepopup')).toBeTruthy()
+    unmount()
+    expect(document.body.querySelector('.p-codepopup')?true:false).toBeFalsy()
+})
